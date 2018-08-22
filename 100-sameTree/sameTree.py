f83211a5b35bc6484818e7f12a590cbd1e7164e3
cqxmzhc <ldzslds@gmail.com>
2018-08-22 12:54:35 +0000

    sameTree

---
+++ 100-sameTree/sameTree.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- encoding:utf-8 -*-
+
+import os
+import sys
+project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, project_dir)
+
+from common.common import *
+
+# Definition for a binary tree node.
+
+
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+
+        # 同时为None
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        else:
+            return p is q
+
+
+def main():
+    s = Solution()
+    p = [1, 2, 1]
+    q = [1, 1, 2]
+    print(s.isSameTree(p, q))
+
+
+if __name__ == '__main__':
+    main()
