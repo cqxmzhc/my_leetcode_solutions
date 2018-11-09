560f541451f242aaaa7a5c565aa26ef70f5b469e
cqxmzhc <ldzslds@gmail.com>
2018-11-09 17:51:39 +0000

    ConvertBSTtoGreaterTree

---
+++ 538-ConvertBSTtoGreaterTree/ConvertBSTtoGreaterTree.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import copy
+import os
+import sys
+
+
+project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, project_dir)
+from common.common import TreeNode, buildTestTree
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def __init__(self):
+        self.total = 0
+
+    def convertBST(self, root):
+        """
+        :type root: TreeNode
+        :rtype: TreeNode
+        """
+        if root:
+            self.convertBST(root.right)
+            self.total += root.val
+            root.val = self.total
+            self.convertBST(root.left)
+        return root
+
+
+if __name__ == '__main__':
+    pass
