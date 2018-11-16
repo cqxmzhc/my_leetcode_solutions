589a850e1cdbb1d94dca250cc5d978042eee27a8
cqxmzhc <ldzslds@gmail.com>
2018-11-16 17:41:43 +0000

    DiameterofBinaryTree

---
+++ 543-DiameterofBinaryTree/DiameterofBinaryTree.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import copy
+import os
+import sys
+
+from common.common import TreeNode, buildTestTree
+
+project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, project_dir)
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+
+class Solution(object):
+    def diameterOfBinaryTree(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+
+
+if __name__ == '__main__':
+    pass
