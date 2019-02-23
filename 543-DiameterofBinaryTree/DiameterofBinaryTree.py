f11dcdacfd124e556b7233c20918b5d3cfa5c96b
cqxmzhc <ldzslds@gmail.com>
2019-02-23 17:10:05 +0800

    DiameterofBinaryTree

--- 543-DiameterofBinaryTree/DiameterofBinaryTree.py
+++ 543-DiameterofBinaryTree/DiameterofBinaryTree.py
@@ -25,6 +25,7 @@ class Solution(object):
         """
         :type root: TreeNode
         :rtype: int
+        时间复杂度: O(N^2)
         """
         if not root:
             return 0
@@ -51,7 +52,33 @@ class Solution(object):
         depth_of_tree = depth_of_left_tree if depth_of_left_tree>depth_of_right_tree else depth_of_right_tree
         return depth_of_tree +1
 
+    
+    def diameterOfBinaryTreeBetter(self, root):
+        """
+        :type root: TreeNode
+        :rtype: int
+        """
+        if not root:
+            return 0
 
+        # 记录遍历过程中当前子树的最大直径
+        self.dia = 0
+
+        def depthOfBinaryTree(root):
+            if not root:
+                return 0
+
+            depth_of_left_tree = depthOfBinaryTree(root.left)
+            depth_of_right_tree = depthOfBinaryTree(root.right)
+
+            # 从下往上, 树的直径=左子树深度+右子树深度
+            if depth_of_left_tree + depth_of_right_tree > self.dia:
+                self.dia = depth_of_left_tree + depth_of_right_tree
+
+            return max(depth_of_left_tree, depth_of_right_tree) + 1
+
+        depthOfBinaryTree(root)
+        return self.dia
 
 if __name__ == '__main__':
     pass
