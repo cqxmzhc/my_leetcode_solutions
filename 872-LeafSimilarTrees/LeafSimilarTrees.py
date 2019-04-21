3f3adbf06a27e59ab07234eaae2f7f976ac2be22
cqxmzhc <ldzslds@gmail.com>
2019-04-21 19:50:41 +0800

    LeafSimilarTrees

---
+++ 872-LeafSimilarTrees/LeafSimilarTrees.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import copy
+import os
+import sys
+
+from common.common import TreeNode, buildTestTree
+
+project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, project_dir)
+
+
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution(object):
+    def leafSimilar(self, root1, root2):
+        """
+        :type root1: TreeNode
+        :type root2: TreeNode
+        :rtype: bool
+        """
+        def dfs(root, leafs):
+            if not root.left and not root.right:
+                leafs.append(root.val)
+                return
+            if root.left:
+                dfs(root.left, leafs)
+            if root.right:
+                dfs(root.right, leafs)
+
+        left_leafs = []
+        dfs(root1, left_leafs)
+
+        right_leafs = []
+        dfs(root2, right_leafs)
+
+        return left_leafs == right_leafs
+
+
+if __name__ == '__main__':
+    pass
