e86943783aeb937973a15ea22746283b6d8af716
cqxmzhc <ldzslds@gmail.com>
2019-07-17 16:34:51 +0800

    HouseRobber

---
+++ 198-HouseRobber/HouseRobber.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import copy
+import os
+import sys
+
+from common.common import TreeNode, buildTestTree
+
+project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, project_dir)
+
+
+class Solution(object):
+    def rob(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        if not nums:
+            return 0
+        if len(nums) == 1:
+            return nums[0]
+
+        dp = [0] * len(nums)
+        dp[0] = nums[0]
+        dp[1] = max(nums[0], nums[1])
+        for i in range(2, len(nums)):
+            dp[i] = max(dp[i-2]+nums[i], dp[i-1])
+
+        return dp[-1]
+
+
+if __name__ == '__main__':
+    pass
