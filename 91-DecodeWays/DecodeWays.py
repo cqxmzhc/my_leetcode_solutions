dbca28cb9db60358d0063bf0a287271860f6bc95
cqxmzhc <ldzslds@gmail.com>
2019-07-21 19:19:33 +0800

    DecodeWays

--- 91-DecodeWays/DecodeWays.py
+++ 91-DecodeWays/DecodeWays.py
@@ -36,6 +36,27 @@ class Solution(object):
 
         return dp[-1]
 
+     def numDecodingsOptimize(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        dp = [0] * len(s)+1
+        # 哨兵位
+        dp[0] = 1
+        dp[1] = 1 if int(s[0]) > 0 else 0
+        for i in range(2, len(s)+1):
+            one = int(s[i-1])
+            two = int(s[i-2:i])
+
+            if one >=1:
+                dp[i] += dp[i-1]
+            if two >= 10 and two <=26:
+                dp[i] += dp[i-2]
+
+        return dp[-1]
+
 
 if __name__ == '__main__':
     pass
