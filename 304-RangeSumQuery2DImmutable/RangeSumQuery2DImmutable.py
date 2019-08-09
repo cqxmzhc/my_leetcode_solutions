56c6d6aa1e6d95a98102fef7439e3ba10aa64ac9
cqxmzhc <ldzslds@gmail.com>
2019-08-09 13:02:23 +0800

    RangeSumQuery2DImmutable

--- 304-RangeSumQuery2DImmutable/RangeSumQuery2DImmutable.py
+++ 304-RangeSumQuery2DImmutable/RangeSumQuery2DImmutable.py
@@ -23,11 +23,20 @@ class NumMatrix(object):
         if col_len == 0:
             return
 
-        self.dp = [[0 for j in range(0, col_len+1)] for i in range(0, row_len)]
-        self.dp[0][0] = matrix[0][0]
+        # method 1
+        # self.dp = [[0 for j in range(0, col_len+1)] for i in range(0, row_len)]
+        # self.dp[0][0] = matrix[0][0]
+        # for i in range(0, row_len):
+        #     for j in range(0, col_len):
+        #         self.dp[i][j+1] = self.dp[i][j] + matrix[i][j]
+
+        # method2
+        # self.dp = [[0 for j in range(0, col_len+1)] for i in range(0, row_len+1)]
+        self.dp = [[0]*(col_len+1) for _ in range(row_len+1)]
+
         for i in range(0, row_len):
             for j in range(0, col_len):
-                self.dp[i][j+1] = self.dp[i][j] + matrix[i][j]
+                self.dp[i+1][j+1] = self.dp[i+1][j] + self.dp[i][j+1] + matrix[i][j] - self.dp[i][j]
 
     def sumRegion(self, row1, col1, row2, col2):
         """
@@ -38,10 +47,12 @@ class NumMatrix(object):
         :rtype: int
         """
         sum = 0
-        for i in range(row1, row2+1):
-            sum += self.dp[i][col2+1] - self.dp[i][col1]
+        # method1
+        # for i in range(row1, row2+1):
+        #     sum += self.dp[i][col2+1] - self.dp[i][col1]
 
-        return sum
+        # return sum
+        return self.dp[row2+1][col2+1] - self.dp[row1][col2+1] - self.dp[row2+1][col1] + self.dp[row1][col1]
 
 
 # Your NumMatrix object will be instantiated and called as such:
