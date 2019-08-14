c3d8e1d4896abbe4855a6012b09793c098eaa1bb
cqxmzhc <ldzslds@gmail.com>
2019-08-14 13:17:46 +0800

    CountingBits

--- 338-CountingBits/CountingBits.py
+++ 338-CountingBits/CountingBits.py
@@ -30,6 +30,13 @@ class Solution(object):
 
         return res
 
+    def countBitsDP(self,num):
+        dp = [0] * (num+1)
+        for i in range(1, num+1):
+            dp[i] = dp[i/2] + i%2
+
+        return dp
+
 
 if __name__ == '__main__':
     pass
