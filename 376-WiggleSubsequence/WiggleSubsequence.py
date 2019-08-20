887f9b0e1ef15cd61b826c87f402a5c0ede3cc66
cqxmzhc <ldzslds@gmail.com>
2019-08-20 20:03:38 +0800

    WiggleSubsequence

--- 376-WiggleSubsequence/WiggleSubsequence.py
+++ 376-WiggleSubsequence/WiggleSubsequence.py
@@ -31,6 +31,32 @@ class Solution(object):
 
         return max(up + down)
 
+    def wiggleMaxLength(self, nums):
+        length = len(nums)
+        if length < 1:
+            return length
+
+        # 长度至少为1
+        # 索引为i时最大子序列长度
+        up = [1] * length
+        down = [1] * length
+
+        for i in range(1, length):
+            if nums[i] > nums[i-1]:
+                # 假设down[i-1] 以nums[j]结尾
+                # 1. nums[j] < nums[i-1] < nums[i], up[i] = down[i-1] + 1
+                # 2. nums[j] >= nums[i-1], 必然存在以nums[i-1]结尾的序列长度等于down[i-1], up[i] = down[i-1]
+                up[i] = down[i-1] + 1
+                down[i] = down[i-1]
+            elif nums[i] < nums[i-1]:
+                down[i] = up[i-1] + 1
+                up[i] = up[i-1]
+            else:
+                down[i] = down[i-1]
+                up[i] = up[i-1]
+
+        return max(up + down)
+
 
 if __name__ == '__main__':
     s = Solution()
