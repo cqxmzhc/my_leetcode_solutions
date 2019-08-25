6e772f22f716ad513cd3c9e4b071023701e70985
cqxmzhc <ldzslds@gmail.com>
2019-08-25 18:35:46 +0800

    PartitionEqualSubsetSum

---
+++ 416-PartitionEqualSubsetSum/PartitionEqualSubsetSum.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, project_dir)
+
+
+class Solution(object):
+    def canPartition(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        if not nums:
+            return True
+        sum_all = sum(nums)
+        # 是否是偶数
+        if sum_all & 1 == 1:
+            return False
+        sum_half = sum_all / 2
+
+        # dp[i][j]表示是否能从前i个元素中算出和为j的子序列
+        dp = [[False for j in range(sum_half+1)] for i in range(len(nums))]
+
+        # base case
+        # 不选择任何元素
+        for i in range(len(nums)):
+            dp[i][0] = True
+
+        # 状态转移方程
+        # dp[i][j] = dp[i-1][j] 不选择元素nums[i]
+        # dp[i][j] = dp[i-1][j-nums[i]] 选择元素nums[i]
+        for i in range(1, len(nums)):
+            for j in range(1, sum_half+1):
+                dp[i][j] = dp[i-1][j] or dp[i-1][j-nums[i]]
+
+        return dp[-1][-1]
+
+
+if __name__ == '__main__':
+    pass
