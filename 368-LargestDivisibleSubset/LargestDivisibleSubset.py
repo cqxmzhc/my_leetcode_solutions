8da0fa24ab165af7dfacd1e287c2407925bfd399
cqxmzhc <ldzslds@gmail.com>
2019-08-25 18:24:54 +0800

    LargestDivisibleSubset

--- 368-LargestDivisibleSubset/LargestDivisibleSubset.py
+++ 368-LargestDivisibleSubset/LargestDivisibleSubset.py
@@ -10,52 +10,36 @@ sys.path.insert(0, project_dir)
 
 
 class Solution(object):
-    def largestDivisibleSubset(self, nums):
+    def canPartition(self, nums):
         """
         :type nums: List[int]
-        :rtype: List[int]
-        T[n]: nums[n]结尾的满足条件的子序列的最大长度
-        T[n] = max(T[n-1], T[n-1] + 1: 如果nums[n]能被nums[n-1]结尾的满足条件的子序列中的最大值整除/能整除最小值)
+        :rtype: bool
         """
-        length = len(nums)
-        if length < 1:
-            return nums
+        if not nums:
+            return True
+        sum_all = sum(nums)
+        # 是否是偶数
+        if sum_all & 1 == 1:
+            return False
+        sum_half = sum_all / 2
 
-        nums.sort()
-        import pdb
-        pdb.set_trace()
-        # 保存以第i个元素结尾满足条件的最大子序列长度
-        dp = [1] * length
-        # 保存满足条件的子序列元素在原序列中的索引关系
-        parent = [-1] * length
+        # dp[i][j]表示是否能从前i个元素中算出和为j的子序列
+        dp = [[False for j in range(sum_half+1)] for i in range(len(nums))]
 
-        # 满足条件的子序列最大长度
-        max_size = 0
-        # 满足条件的子序列最后一个元素在原始序列中的索引
-        max_size_index = 0
+        # base case
+        # 不选择任何元素
+        for i in range(len(nums)):
+            dp[i][0] = True
 
-        for i in range(1, length):
-            for j in range(i-1, -1, -1):
-                # nums已经排序，只需要检查是否能被子序列中的最大值整除
-                if nums[i] % nums[j] == 0:
-                    if dp[j] + 1 > dp[i]:
-                        dp[i] = dp[j] + 1
-                        # 子序列中的索引关系 j -> i
-                        parent[i] = j
+        # 状态转移方程
+        # dp[i][j] = dp[i-1][j] 不选择元素nums[i]
+        # dp[i][j] = dp[i-1][j-nums[i]] 选择元素nums[i]
+        for i in range(1, len(nums)):
+            for j in range(1, sum_half+1):
+                dp[i][j] = dp[i-1][j] or dp[i-1][j-nums[i]]
 
-            if max_size < dp[i]:
-                max_size = dp[i]
-                max_size_index = i
-
-        parent_index = max_size_index
-        result = []
-        while parent_index >= 0:
-            result.insert(0, nums[parent_index])
-            parent_index = parent[parent_index]
-
-        return result
+        return dp[-1][-1]
 
 
 if __name__ == '__main__':
-    s = Solution()
-    print(s.largestDivisibleSubset([3, 2, 1, 5]))
+    pass
