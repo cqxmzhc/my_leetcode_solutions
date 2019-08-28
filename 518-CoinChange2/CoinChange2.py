96f78b1d31215d242d727f2f2ef76e2b45060089
cqxmzhc <ldzslds@gmail.com>
2019-08-28 17:03:02 +0800

    CoinChange2

---
+++ 518-CoinChange2/CoinChange2.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+project_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, project_dir)
+
+
+class Solution(object):
+    def change(self, amount, coins):
+        """
+        :type amount: int
+        :type coins: List[int]
+        :rtype: int
+        """
+        dp = [0] * (amount + 1)
+        dp[0] = 1
+        for coin in coins:
+            for i in range(coin, amount+1):
+                dp[i] += dp[i-coin]
+
+        return dp[-1]
+
+
+if __name__ == '__main__':
+    s = Solution()
+    res = s.change(5, [1, 2, 4])
+    print(res)
