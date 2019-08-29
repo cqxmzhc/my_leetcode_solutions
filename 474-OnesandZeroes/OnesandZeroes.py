ff9abfb6382e30479c2c9ed2f6c961d38eac7bc7
cqxmzhc <ldzslds@gmail.com>
2019-08-29 10:56:49 +0800

    OnesandZeroes

--- 474-OnesandZeroes/OnesandZeroes.py
+++ 474-OnesandZeroes/OnesandZeroes.py
@@ -17,7 +17,7 @@ class Solution(object):
         0-1背包问题, strs数组中的数字作为物体, m和n分别作为需要被填充的背包
         """
         dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
-        print(dp)
+        # print(dp)
         for num in strs:
             one_count = 0
             zero_count = 0
@@ -27,9 +27,16 @@ class Solution(object):
                 else:
                     zero_count += 1
 
-            for j in range(one_count, n+1):
-                for k in range(zero_count, m+1):
-                    dp[k][j] = max(dp[k][j], dp[m-k][n-j] + 1)
+            # 循环到此处表示的是我们考虑选择当前字符串
+            # 对于二维数组dp， 如果更新顺序是从左上角(top left)到右下角(bottom right), 后续的元素更新会使用的前面已经被更新的元素的值(选择当前字符串已经+1), 所以会导致重复计算
+            # 从右下角更新到左上角，使用的更新元素是在当前选择字符串下未被更新的值
+            # for k in range(zero_count, m+1):
+            #     for j in range(one_count, n+1):
+            #         dp[k][j] = max(dp[k][j], dp[k-zero_count][j-one_count])
+
+            for k in range(m, zero_count-1, -1):
+                for j in range(n, one_count-1, -1):
+                    dp[k][j] = max(dp[k][j], dp[k-zero_count][j-one_count] + 1)
 
         return dp[m][n]
 
